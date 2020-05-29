afd31142fa766a493c1297aaa7f32a8e49d70e92
Piotr Łępa <piotr.lepa@altconnect.pl>
2020-05-29 17:38:37 +0200

    update url

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,19 +10,19 @@ export class DataService {
   }
 
   getAll() {
-    return this.http.get(this.url + '/api/posts');
+    return this.http.get(this.url + '/posts');
   }
 
   getById(id) {
-    return this.http.get(this.url + `/api/posts/${id}`);
+    return this.http.get(this.url + `/posts/${id}`);
   }
 
   getByText(data) {
-    return this.http.post(this.url + '/api/posts/', data);
+    return this.http.post(this.url + '/posts/', data);
   }
 
   createOrUpdate(post) {
-    return this.http.post(`${this.url}/api/posts`, post);
+    return this.http.post(`${this.url}/posts`, post);
   }
 }
 
